f8e6411e5082844b57823aab916036d187f2695b
Rui Yong <ngruiyong@hotmail.com>
2023-09-06 23:23:29 +0800

    feat(Button): add loading

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -2,17 +2,27 @@ import { FunctionComponent, ReactNode, ButtonHTMLAttributes } from "react";
 
 type TButtonProps = {
   children: ReactNode;
+  loading?: boolean;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button: FunctionComponent<TButtonProps> = ({
   children,
+  loading,
+  disabled,
   ...buttonProps
 }) => {
   return (
     <button
-      className="rounded py-2 px-3 shadow-md active:shadow-none   flex items-center "
+      className="rounded py-2 px-3 shadow-lg disabled:opacity-10 disabled:cursor-not-allowed active:shadow-inner flex items-center "
+      disabled={loading || disabled}
       {...buttonProps}
     >
+      {loading && (
+        <svg
+          className="animate-spin h-5 w-5 mr-2 border-solid border-2 border-b-0 border-r-0 border-black rounded-full"
+          viewBox="0 0 24 24"
+        ></svg>
+      )}
       {children}
     </button>
   );
