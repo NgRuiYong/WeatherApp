17c018acc5b4079ad7b1a731505367b4fd555e60
Rui Yong <ngruiyong@hotmail.com>
2023-09-06 23:29:09 +0800

    feat(page): add search functionality and display results

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 "use client";
-import { FunctionComponent } from "react";
 import Button from "./components/Button/Button";
 import Header from "./components/Header/Header";
+import WeatherInformation from "./components/WeatherInformation/WeatherInformation";
+
+import { FunctionComponent, useState } from "react";
+import {
+  TReverseGeoCodingResponse,
+  TWeatherFormattedResponse,
+  getReverseGeoCoding,
+  getWeather,
+} from "./utils/networkUtils";
 import { useForm } from "react-hook-form";
+import InputGroup from "./components/InputGroup/InputGroup";
 
 type SearchFormState = {
   city: string;
@@ -10,14 +19,38 @@ type SearchFormState = {
 };
 
 const MainPage: FunctionComponent = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<SearchFormState>();
+  const { register, handleSubmit, formState } = useForm<SearchFormState>();
+  const [isSearching, setIsSearching] = useState<boolean>(false);
+  const [searchResult, setSearchResult] = useState<
+    TWeatherFormattedResponse | undefined
+  >(undefined);
 
   const submitFn = async (data: SearchFormState) => {
-    console.log({ data });
+    setIsSearching(true);
+    try {
+      console.log({ data });
+
+      const geoCodeResponse: TReverseGeoCodingResponse[] =
+        await getReverseGeoCoding({
+          city: data.city,
+          countryCode: data?.countryCode,
+        });
+
+      if (geoCodeResponse.length === 0) {
+        setIsSearching(false);
+        return;
+      }
+
+      const geoCode = geoCodeResponse[0];
+
+      const weatherResponse: TWeatherFormattedResponse = await getWeather({
+        lat: geoCode.lat,
+        lon: geoCode.lon,
+      });
+      setSearchResult(weatherResponse);
+    } catch (e) {}
+
+    setIsSearching(false);
   };
 
   return (
@@ -27,35 +60,33 @@ const MainPage: FunctionComponent = () => {
           className="flex flex-wrap gap-3"
           onSubmit={handleSubmit(submitFn)}
         >
-          <div className="inputGroup">
-            <label htmlFor="city">City: </label>
-            <input
-              className="form-input"
-              id="city"
-              type="text"
-              {...register("city")}
-              required
-            />
-          </div>
-          <div className="inputGroup">
-            <label className="min-w-[65px] sm:min-w-fit" htmlFor="country">
-              Country:
-            </label>
-            <input
-              className="form-input"
-              id="countryCode"
-              {...register("countryCode")}
-              type="text"
-              required
-            />
-          </div>
+          <InputGroup
+            label="City: "
+            type="text"
+            disabled={isSearching}
+            required
+            {...register("city")}
+          />
+          <InputGroup
+            label="Country: "
+            type="text"
+            disabled={isSearching}
+            required
+            {...register("countryCode")}
+          />
           <div className="inputGroupButton">
-            <Button type="submit">Search</Button>
-            <Button type="reset">Clear</Button>
+            <Button loading={isSearching} type="submit">
+              Search
+            </Button>
+            <Button disabled={isSearching} type="reset">
+              Clear
+            </Button>
           </div>
         </form>
       </section>
-      <section className="mb-7">Insert loaded data Here</section>
+      <section className="mb-7 pl-6">
+        {searchResult && <WeatherInformation {...searchResult} />}
+      </section>
       <section className="mb-7">
         <Header underline>Search History</Header>
       </section>
