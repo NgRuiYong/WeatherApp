a24b3c2da389d95fc5d5857bdb542ca991d91e16
Rui Yong <ngruiyong@hotmail.com>
2023-09-06 16:52:49 +0800

    config: install forms plugins, React Hook forms and ky

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import FormsPlugin from "@tailwindcss/forms";
 
 const config: Config = {
   content: [
@@ -9,6 +10,6 @@ const config: Config = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [FormsPlugin],
 };
 export default config;
