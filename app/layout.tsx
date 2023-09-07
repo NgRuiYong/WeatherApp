58e0109e063b5138a45911d08e421a9f225d0e93
Rui Yong <ngruiyong@hotmail.com>
2023-09-07 11:40:04 +0800

    feat(page): add search history and add error handling for http failure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import "react-toastify/dist/ReactToastify.min.css";
 import type { Metadata } from "next";
 import Header from "@/components/Header/Header";
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import Button from "./components/Button/Button";
 import Header from "./components/Header/Header";
 import WeatherInformation from "./components/WeatherInformation/WeatherInformation";
-
+import { toast, ToastContainer } from "react-toastify";
 import { FunctionComponent, useState } from "react";
 import {
   TReverseGeoCodingResponse,
@@ -12,10 +12,17 @@ import {
 } from "./utils/networkUtils";
 import { useForm } from "react-hook-form";
 import InputGroup from "./components/InputGroup/InputGroup";
+import { convertUTCToTimeZone } from "./utils/utils";
+import { MagnifyingGlassIcon, TrashIcon } from "@heroicons/react/24/outline";
 
 type SearchFormState = {
   city: string;
   countryCode: string;
+};
+
+type TSearchHistory = {
+  searchForm: SearchFormState;
+  timestamp: number;
 };
 
 const MainPage: FunctionComponent = () => {
@@ -25,11 +32,21 @@ const MainPage: FunctionComponent = () => {
   const [searchResult, setSearchResult] = useState<
     TWeatherFormattedResponse | undefined
   >(undefined);
+  const [searchHistory, setSearchHistory] = useState<TSearchHistory[]>([]);
 
   const submitFn = async (data: SearchFormState) => {
-    setIsSearching(true);
     setHasNoResults(false);
     setSearchResult(undefined);
+    const formattedSearchHistory: TSearchHistory = {
+      searchForm: data,
+      timestamp: Date.now(),
+    };
+    setSearchHistory([formattedSearchHistory, ...searchHistory]);
+    await getWeatherInformation(data);
+  };
+
+  const getWeatherInformation = async (data: SearchFormState) => {
+    setIsSearching(true);
     try {
       const geoCodeResponse: TReverseGeoCodingResponse[] =
         await getReverseGeoCoding({
@@ -51,14 +68,32 @@ const MainPage: FunctionComponent = () => {
       });
       setSearchResult(weatherResponse);
     } catch (e) {
+      toast.error("Error! Failed to get weather information.", {
+        position: toast.POSITION.TOP_CENTER,
+      });
       setHasNoResults(true);
       setIsSearching(false);
     }
     setIsSearching(false);
   };
 
+  const handleOnClickSearchHistory = async (
+    searchFormData: SearchFormState
+  ) => {
+    await submitFn(searchFormData);
+  };
+
+  const handleOnDeleteSearchHistory = (index: number) => {
+    const updatedArray: TSearchHistory[] = [
+      ...searchHistory.slice(0, index),
+      ...searchHistory.slice(index + 1),
+    ];
+    setSearchHistory(updatedArray);
+  };
+
   return (
     <div>
+      <ToastContainer autoClose={3000} />
       <section className="mb-4">
         <form
           className="flex flex-wrap gap-3"
@@ -72,7 +107,7 @@ const MainPage: FunctionComponent = () => {
             {...register("city")}
           />
           <InputGroup
-            label="Country: "
+            label="Country Code: "
             type="text"
             disabled={isSearching}
             required
@@ -100,8 +135,47 @@ const MainPage: FunctionComponent = () => {
           </div>
         )}
       </section>
-      <section className="mb-7">
+      <section className="mb-7 mt-2">
         <Header underline>Search History</Header>
+        <div className="flex flex-col">
+          {searchHistory.length === 0 ? (
+            <div className="w-full h-24 flex items-center justify-center">
+              No Record
+            </div>
+          ) : (
+            searchHistory.map(({ searchForm, timestamp }, index) => (
+              <div
+                key={index}
+                className="flex justify-between border-b-2 border-gray-200 py-3"
+              >
+                <div>{`${index + 1}. ${searchForm.city}, ${
+                  searchForm.countryCode
+                }`}</div>
+                <div className="flex items-center gap-2">
+                  <span>{convertUTCToTimeZone(timestamp, "hh:mm:ss a")}</span>
+                  <span>
+                    <Button
+                      rounded
+                      className="bg-gray-200 aspect-square px-2 py-2"
+                      onClick={() => handleOnClickSearchHistory(searchForm)}
+                    >
+                      <MagnifyingGlassIcon className="h-4 w-4" />
+                    </Button>
+                  </span>
+                  <span>
+                    <Button
+                      rounded
+                      className="bg-gray-200 aspect-square px-2 py-2"
+                      onClick={() => handleOnDeleteSearchHistory(index)}
+                    >
+                      <TrashIcon className="h-4 w-4" />
+                    </Button>
+                  </span>
+                </div>
+              </div>
+            ))
+          )}
+        </div>
       </section>
     </div>
   );
